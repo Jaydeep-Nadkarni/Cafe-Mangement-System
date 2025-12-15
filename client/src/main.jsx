d8f9566cd5ca84dc30cc138276658d4ecdbf4791
Jaydeep Nadkarni <jaydeepnadkarni123@gmail.com>
2025-12-15 15:30:47 +0530

    feat: Implement SocketContext and useBranchSocket hook for real-time branch event handling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { CartProvider } from './context/CartContext'
+import { SocketProvider } from './context/SocketContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <SocketProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </SocketProvider>
   </React.StrictMode>,
 )
